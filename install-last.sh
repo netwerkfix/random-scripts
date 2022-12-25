1f37ca29806abb75be651d7fc46f2375885952e1
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 00:53:37 +0100

    Update install-last.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- install-last.sh
+++ install-last.sh
@@ -4,13 +4,13 @@ sudo mkdir -p /etc/sysprep/last-steps
 sleep 1
 cd /etc/sysprep/last-steps/
 sleep 1
-sudo wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/run-last-steps.sh?token=GHSAT0AAAAAAB4X3P6ZMHLLARBVLZLCEXOSY5I4DQA
+sudo wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/run-last-steps.sh
 sleep 1
 chmod 775 run-last-steps.sh
 sleep 1
 cd /etc/systemd/system/
 sleep 2
-wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/run-last-steps.service?token=GHSAT0AAAAAAB4X3P6ZBXIF55STREBDASSWY5I4DGA
+wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/run-last-steps.service
 sleep 1
 chmod 755 run-steps.service
 sleep 1
