99e97331b0ce41ac18fb64e5203d3f587d712a2c
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 00:32:30 +0100

    Create run-last-steps.sh

---
+++ run-last-steps.sh
@@ -0,0 +1,26 @@
+sudo mkdir -p /etc/sysprep/last-steps
+sudo apt update -y
+sleep 1
+sudo apt upgrade -y
+sleep 1
+sudo apt install -y open-vm-tools
+sleep 1
+service rsyslog restart
+sleep 2
+if [ -f /var/log/wtmp ]; then
+    truncate -s0 /var/log/wtmp
+fi
+if [ -f /var/log/lastlog ]; then
+    truncate -s0 /var/log/lastlog
+fi
+sleep 1
+rm -rf /tmp/*
+sleep 1
+rm -rf /var/tmp/*
+sleep 2
+rm -f /etc/ssh/ssh_host_*
+sleep 2
+dhclient -r
+sleep 3
+systemctl disable --now run-last-steps.service
+sudo rm -f /etc/sysprep/last-steps
