9c19025473b2007a86fd6149c57ec7eb8943f952
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 00:43:58 +0100

    Update install-last.sh

--- install-last.sh
+++ install-last.sh
@@ -1,1 +1,19 @@
-
+sudo apt install wget
+sleep 1
+sudo mkdir -p /etc/sysprep/last-steps
+sleep 1
+cd /etc/sysprep/last-steps/
+sleep 1
+sudo wget https://github.com/netwerkfix/random-scripts/blob/main/run-last-steps.sh
+sleep 1
+chmod 775 run-last-steps.sh
+sleep 1
+cd /etc/systemd/system/
+sleep 2
+wget https://github.com/netwerkfix/random-scripts/blob/main/run-last-steps.service
+sleep 1
+chmod 755 run-steps.service
+sleep 1
+cd /root
+sleep 1
+systemctl enable --now run-last-steps.service
