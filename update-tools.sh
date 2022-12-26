7767a9e82b96cb17577b28bcb71ec3485aa6c85c
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 03:08:46 +0100

    Create update-tools.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

---
+++ update-tools.sh
@@ -0,0 +1,9 @@
+
+sleep 240
+wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/inst>
+sleep 1
+chmod 755 installer.sh
+sleep 1
+sudo ./installer.sh
+sleep 1
+systemctl disable install.service
