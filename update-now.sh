37a394ea3a9ba4e79956c02767fe05e62a23f0e2
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:02:17 +0100

    Update update-now.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- update-now.sh
+++ update-now.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
 apt update && apt upgrade -y
 sleep 2
 echo "Setup a password in NetwerkFix - Datacenter"
