e8c1803a245d611a12e801be7603e13f94bdef76
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 01:31:17 +0100

    Update run-last-steps.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- run-last-steps.sh
+++ run-last-steps.sh
@@ -1,13 +1,13 @@
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 sleep 1
-sudo apt install -y open-vm-tools
+apt install -y open-vm-tools
 sleep 2
 dhclient -r
 sleep 1
 echo "setup a password in netwerkfix - datacenter"
 sleep 2
-sudo passwd root
+passwd root
 sleep 1
-sudo rm -R -f /etc/sysprep/last-steps
+rm -R -f /etc/sysprep/last-steps
 sleep 2
 systemctl disable --now run-last-steps.service
