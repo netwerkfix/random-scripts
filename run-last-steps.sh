1cbd0f930e0b2499365aa8c81c861105506b377a
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:01:06 +0100

    Update run-last-steps.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- run-last-steps.sh
+++ run-last-steps.sh
@@ -1,7 +1,21 @@
+
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
 apt update && apt upgrade -y
 sleep 1
 apt install -y open-vm-tools
 sleep 2
 dhclient -r
-sleep 1
-rm -R -f /etc/sysprep/last-steps
