264ff9bd805d97d773317cce4abbada8a7755a3a
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 04:32:20 +0100

    Update install-last.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- install-last.sh
+++ install-last.sh
@@ -20,12 +20,16 @@ cd /etc/sysprep/
 sleep 1
 wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/update.sh
 sleep 1
-cd /root
+chmod 755 update.sh
 sleep 2
 cd /etc/sysprep/
 sleep 1
 wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/update-now.sh
 sleep 2
+chmod 755 update-now.sh
+sleep 2
 ./update-now.sh
+sleep 2
+cd /root
 sleep 2
 systemctl enable --now update.service
