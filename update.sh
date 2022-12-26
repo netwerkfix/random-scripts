812dcb7f2aeb0bebefac87ce6411a669480f0e53
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:03:05 +0100

    Update update.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
 #update's every month
 sleep 2628000
 apt update && apt upgrade -y
