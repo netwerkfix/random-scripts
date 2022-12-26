9a2a7de2704bacc3c34454cf60e709a2bac26fee
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:00:27 +0100

    Update install-last.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- install-last.sh
+++ install-last.sh
@@ -1,3 +1,19 @@
+
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
 apt install wget
 sleep 1
 sudo mkdir -p /etc/sysprep/last-steps
