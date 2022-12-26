818a9af75db47eea31798454a85433d3e6d61976
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 03:48:45 +0100

    Update update-tools.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- update-tools.sh
+++ update-tools.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
 sleep 240
 wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/inst>
 sleep 1
