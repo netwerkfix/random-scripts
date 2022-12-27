1309ae0615ab59a7669544ec2a75a636276309a3
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-27 03:22:24 +0100

    Update password.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- password.sh
+++ password.sh
@@ -1,8 +1,11 @@
-sleep 262974383
+clear
+sleep 5
 echo "Setup a password in NetwerkFix - Datacenter"
 sleep 3
 passwd root
-sleep 3
+sleep 130
+echo "waiting 1min 30sec"
+sleep 2
 systemctl disable --now passwd.service
 sleep 5
 rm /etc/systemd/system/passwd.service
