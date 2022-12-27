269ecad10d96bef82e8ddd910d6710e9cf23e71d
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-27 03:27:28 +0100

    Update password.sh
    Signed-off-by: DamienCoop <67594538+netwerkfix@users.noreply.github.com>

--- password.sh
+++ password.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+# this command give the script sudo perms
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
+
 clear
 sleep 5
 echo "Setup a password in NetwerkFix - Datacenter"
